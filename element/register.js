ede6f5411979812c5739de729e2207cda4882737
Peter StJ <regardingscot@gmail.com>
2016-01-20 06:05:48 +0200

    Fixes linter warnings

--- element/register.js
+++ element/register.js
@@ -3,11 +3,15 @@ goog.provide('sm.element.RegisterRenderer');
 
 goog.require('goog.events.EventType');
 goog.require('goog.ui.registry');
+/** @suppress {extraRequire} */
 goog.require('pstj.element.ErrorMsg');
 goog.require('pstj.element.Form');
 goog.require('pstj.element.FormRenderer');
+/** @suppress {extraRequire} */
 goog.require('pstj.material.Button');
+/** @suppress {extraRequire} */
 goog.require('pstj.material.Input');
+/** @suppress {extraRequire} */
 goog.require('pstj.material.Shadow');
 goog.require('sm.template');
 
@@ -31,7 +35,8 @@ sm.element.Register = goog.defineClass(pstj.element.Form, {
   enterDocument: function() {
     goog.base(this, 'enterDocument');
     this.getHandler().listen(this.querySelector('[name="country"]'),
-        goog.events.EventType.CHANGE, this.handleCountryChangeEvent);
+                             goog.events.EventType.CHANGE,
+                             this.handleCountryChangeEvent);
   },
 
   /**
@@ -41,12 +46,12 @@ sm.element.Register = goog.defineClass(pstj.element.Form, {
    * @protected
    */
   handleCountryChangeEvent: function(e) {
-    var select = /** @type {HTMLSelectElement} */(e.target);
+    var select = /** @type {HTMLSelectElement} */ (e.target);
     var value = select.value;
     if (value != 'Unated States') {
-      var s = /** @type {HTMLSelectElement} */(
+      var s = /** @type {HTMLSelectElement} */ (
           this.querySelector('[name="state"]'));
-      s.value = /** @type {HTMLOptionElement} */(s.options.item(0)).value;
+      s.value = /** @type {HTMLOptionElement} */ (s.options.item(0)).value;
     }
   },
 
@@ -65,36 +70,24 @@ sm.element.Register = goog.defineClass(pstj.element.Form, {
 
   /**
    * Expose the form as a model.
-   * @param  {pstj.ds.DtoBase} model
+   * @param  {?pstj.ds.DtoBase=} opt_model
    */
-  updateModel: function(model) {
-    this.updateModelFromElements(model);
-  }
+  updateModel: function(opt_model) { this.updateModelFromElements(opt_model); }
 });
 
 
 /** @extends {pstj.element.FormRenderer} */
 sm.element.RegisterRenderer = goog.defineClass(pstj.element.FormRenderer, {
-  constructor: function() {
-    pstj.element.FormRenderer.call(this);
-  },
+  constructor: function() { pstj.element.FormRenderer.call(this); },
 
   /** @override */
-  generateTemplateData: function(instance) {
-    return {
-      registration: true
-    };
-  },
+  generateTemplateData: function(instance) { return {registration: true}; },
 
   /** @override */
-  getCssClass: function() {
-    return sm.element.RegisterRenderer.CSS_CLASS;
-  },
+  getCssClass: function() { return sm.element.RegisterRenderer.CSS_CLASS; },
 
   /** @override */
-  getTemplate: function(model) {
-    return sm.template.Register(model);
-  },
+  getTemplate: function(model) { return sm.template.Register(model); },
 
   /** @override */
   getErrorMsgElement: function(ctrl) {
@@ -116,11 +109,10 @@ goog.addSingletonGetter(sm.element.RegisterRenderer);
 
 // Register for default renderer.
 goog.ui.registry.setDefaultRenderer(sm.element.Register,
-    sm.element.RegisterRenderer);
+                                    sm.element.RegisterRenderer);
 
 
 // Register decorator factory function.
 goog.ui.registry.setDecoratorByClassName(
-    sm.element.RegisterRenderer.CSS_CLASS, function() {
-      return new sm.element.Register();
-    });
+    sm.element.RegisterRenderer.CSS_CLASS,
+    function() { return new sm.element.Register(); });
